74150b3ed1ac9a5cfbba65e1a480100b1446fc4f
anakpringgabaya-alt <anakpringgabaya@gmail.com>
2026-01-03 21:02:53 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,6 @@
+const games=[
+{title:"Gates of Olympus",image:"https://picsum.photos/300/200?1",link:"#",badge:"Hot"},
+{title:"Sweet Bonanza",image:"https://picsum.photos/300/200?2",link:"#",badge:"Hot"},
+{title:"Starlight Princess",image:"https://picsum.photos/300/200?3",link:"#",badge:"New"},
+{title:"Sugar Rush",image:"https://picsum.photos/300/200?4",link:"#",badge:"Popular"}
+];
