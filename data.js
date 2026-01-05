38f1bae48da6d003deb1f442cfcd574ec055b791
anakpringgabaya-alt <anakpringgabaya@gmail.com>
2026-01-05 19:57:46 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,17 @@
+const games = [
+{
+  name: "8768D",
+  logo: "assets/8768d.png",
+  link: "https://example.com"
+},
+{
+  name: "KAKRP",
+  logo: "assets/kakrp.png",
+  link: "https://example.com"
+},
+{
+  name: "PRAGMATIC PLAY",
+  logo: "assets/pragmatic.png",
+  link: "https://example.com"
+}
+];
