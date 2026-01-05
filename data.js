e59edec3584d7d662d1b40454aac6baa4f345abd
anakpringgabaya-alt <anakpringgabaya@gmail.com>
2026-01-05 20:57:49 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,12 @@
+const games = [
+  { name: "INA777", logo: "ina777.png", link: "https://h5.in858.win/?id=MTcxMDQ5NTI%3D" },
+  { name: "Y89", logo: "y89.png", link: "https://h5.zues188.co/?id=MjA5NzIxNjc%3D" },
+  { name: "SATU777", logo: "satu777.png", link: "https://h5.sasafun.fun/?id=NTExNTMzMTY%3D" },
+  { name: "DAY777", logo: "day777.png", link: "https://h5.day787.fun/?id=OTcwMTE5MjMz" },
+  { name: "GF777", logo: "GF777.png", link: "https://h5.see789.fun/?id=ODMwMTk5MzU2" },
+  { name: "DAN777", logo: "dan777.png", link: "https://h5.dan777e.com/?id=MjY4MDYyMDcy" },
+  { name: "KETUA", logo: "ketua.png", link: "https://h5.ketua7.com/?id=NTA0ODg4Mjc5" },
+  { name: "33ZK", logo: "33zk.png", link: "https://h5.338zk.com/?id=MjM0NTMxNDcx" },
+  { name: "AYUWIN", logo: "ayuwin.png", link: "https://h5.ayuwin.win/?id=OTk4ODI5NjQw" },
+  { name: "RP777", logo: "rp777.png", link: "https://h5.we878.fun/?id=MTAwNzkxNzQ%3D" }
+];
