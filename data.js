2d95654cbff331150afc00c752914421212c9f5e
anakpringgabaya-alt <anakpringgabaya@gmail.com>
2026-01-05 21:55:53 +0800

    Update data.js

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ const games = [
 {name:"INA777",domain:"h5.in858.win",link:"https://h5.in858.win/?id=MTcxMDQ5NTI="},
 {name:"Y89",domain:"h5.zues188.co",link:"https://h5.zues188.co/?id=MjA5NzIxNjc="},
 {name:"SATU777",domain:"h5.sasafun.fun",link:"https://h5.sasafun.fun/?id=NTExNTMzMTY="},
-{name:"DAY777",domain:"h5.day787.fun",link:"https://h5.day787.fun/?id=OTcwMTE5MjMz"},
+{name:"DAY777",domain:"https://h5.8768f.com",link:"https://h5.8768f.com/?id=NTQ2MDQzMzcz"},
 {name:"GF777",domain:"h5.see789.fun",link:"https://h5.see789.fun/?id=ODMwMTk5MzU2"},
 {name:"DAN777",domain:"h5.dan777e.com",link:"https://h5.dan777e.com/?id=MjY4MDYyMDcy"},
 {name:"KETUA",domain:"h5.ketua7.com",link:"https://h5.ketua7.com/?id=NTA0ODg4Mjc5"},
